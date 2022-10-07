ed4b8a650e81db066c14ea74252f3c9fad74bb91
alsrlrk123 <alsrlrk123@naver.com>
2022-10-07 11:56:40 +0900

    1

---
+++ src/components/MainVisual.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="visual"></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.visual{
+    width: 100%;
+    height: 300px;
+    background-color: pink;
+}
+</style>
